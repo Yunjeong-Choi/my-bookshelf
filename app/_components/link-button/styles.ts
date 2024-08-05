fb108f6508948bc57efb57f493cc14df93cba410
Yunjeong Choi <yunjeongchoi@Yunjeongui-MacBookPro.local>
2024-08-06 00:29:37 +0900

    [MBSH-3] 로그인 페이지 추가

--- app/_components/link-button/styles.ts
+++ app/_components/link-button/styles.ts
@@ -6,6 +6,7 @@ import styled from "styled-components";
 import color from "@/shared/styles/color";
 
 export const Container = styled(Link)`
+  box-shadow: 0.4rem 0.4rem 0.4rem rgba(0, 0, 0, 0.25);
   border-radius: 1rem;
   background-color: ${color.brown700};
   color: ${color.warmgray100};
